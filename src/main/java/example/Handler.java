5c13b88eb13d35a4979cc6bb2a45da6ce03265fc
AAEU <amanda.sousa@gft.com>
2021-04-28 10:39:10 -0300

    Listagem de objetos de uma bucket s3

--- src/main/java/example/Handler.java
+++ src/main/java/example/Handler.java
@@ -2,12 +2,12 @@ package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.util.StringUtils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -15,22 +15,20 @@ import java.util.List;
 import java.util.Map;
 
 // example.Handler value: example.example.Handler
-public class Handler implements RequestHandler<Map<String,String>, String>{
+public class Handler implements RequestHandler<Map<String, String>, List> {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    static String bucketName = "meubucketlambdaconsumer";
+    static String bucketName = "bucketlistar";
 
     @Override
-    public String handleRequest(Map<String,String> event, Context context)
-    {
+    public List handleRequest(Map<String, String> event, Context context) {
 
         //Conectar numa s3 e listar os arquivos
         final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
         ObjectListing ol = s3.listObjects(bucketName);
-        List<S3ObjectSummary> objects = ol.getObjectSummaries();
-        for (S3ObjectSummary os : objects) {
-            System.out.println("Arquivos: " + os.getBucketName());
-        }
 
-        return ol.getBucketName();
+        List s3objects = s3.listObjects(bucketName).getObjectSummaries();
+
+
+        return s3objects;
     }
 }
