184807763a41ddbc34ee79072c0c723ca37de7f5
AAEU <amanda.sousa@gft.com>
2021-04-28 14:30:23 -0300

    Verificando se tem arquivos novos na bucket nas últimas 24h 1 dia

--- src/main/java/example/Handler.java
+++ src/main/java/example/Handler.java
@@ -11,24 +11,40 @@ import com.amazonaws.util.StringUtils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.util.List;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 // example.Handler value: example.example.Handler
-public class Handler implements RequestHandler<Map<String, String>, List> {
+public class Handler implements RequestHandler<Map<String, String>, Boolean> {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
     static String bucketName = "bucketlistar";
-
+    final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+    Boolean result;
     @Override
-    public List handleRequest(Map<String, String> event, Context context) {
+    public Boolean handleRequest(Map<String, String> event, Context context) {
 
-        //Conectar numa s3 e listar os arquivos
-        final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
         ObjectListing ol = s3.listObjects(bucketName);
 
+        //Conectar numa s3 e listar os arquivos
+/*
         List s3objects = s3.listObjects(bucketName).getObjectSummaries();
+*/
+        LocalDateTime now = LocalDateTime.now().minusDays(1);
 
+        for (S3ObjectSummary objectSummary : ol.getObjectSummaries()) {
 
-        return s3objects;
+            if(StringUtils.fromDate(objectSummary.getLastModified()).compareTo(String.valueOf(now)) > 0){
+                result = true;
+            }
+            else{
+                result = false;
+            }
+
+            /*result = objectSummary.getKey() + "\n" +
+                    objectSummary.getSize() + "\n" +
+                    StringUtils.fromDate(objectSummary.getLastModified());*/
+        }
+
+        return result;
     }
 }
