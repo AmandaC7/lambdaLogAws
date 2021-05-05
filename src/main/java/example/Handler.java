1e7727c2da6cf7b1477c8ac48707745b3893ea64
AAEU <amanda.sousa@gft.com>
2021-05-05 16:01:00 -0300

    Verificação de arquivos s3 das últimas 24h e alarme

--- src/main/java/example/Handler.java
+++ src/main/java/example/Handler.java
@@ -1,6 +1,7 @@
 package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import com.amazonaws.services.s3.AmazonS3;
@@ -11,6 +12,9 @@ import com.amazonaws.util.StringUtils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
+
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -19,32 +23,32 @@ public class Handler implements RequestHandler<Map<String, String>, Boolean> {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
     static String bucketName = "bucketlistar";
     final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+    final AmazonCloudWatch cw = AmazonCloudWatchClientBuilder.defaultClient();
     Boolean result;
+
     @Override
     public Boolean handleRequest(Map<String, String> event, Context context) {
-
         ObjectListing ol = s3.listObjects(bucketName);
-
-        //Conectar numa s3 e listar os arquivos
-/*
-        List s3objects = s3.listObjects(bucketName).getObjectSummaries();
-*/
         LocalDateTime now = LocalDateTime.now().minusDays(1);
+        LambdaLogger logger = context.getLogger();
+        String response = new String("SUCCESS");
 
         for (S3ObjectSummary objectSummary : ol.getObjectSummaries()) {
-
-            if(StringUtils.fromDate(objectSummary.getLastModified()).compareTo(String.valueOf(now)) > 0){
+            if (StringUtils.fromDate(objectSummary.getLastModified()).compareTo(String.valueOf(now)) > 0) {
+                logger.log(response);
                 result = true;
             }
             else{
+                logger.log(response = "ERROR");
                 result = false;
             }
-
-            /*result = objectSummary.getKey() + "\n" +
-                    objectSummary.getSize() + "\n" +
-                    StringUtils.fromDate(objectSummary.getLastModified());*/
+        }
+        if(result == null){
+            logger.log(response = "ERROR");
+            return result = false;
         }
 
+        System.out.println("The result is: " + result);
         return result;
     }
 }
